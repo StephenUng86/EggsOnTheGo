4f8250731e4971e7ff2aaca970c693379f483c5e
StephenUng86 <95722529+StephenUng86@users.noreply.github.com>
2021-12-09 16:25:21 -0700

    eggsonthego

---
+++ action_page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0041)https://webdevbasics.net/scripts/demo.php -->
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Form Confirmation</title>
+	<link rel="stylesheet" href="./Form Confirmation_files/demo.css" type="text/css">
+  	</head>
+	<body>
+	<h1>Your information has been received.</h1><br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<form action="https://webdevbasics.net/scripts/demo.php#">
+<input type="button" value="Back" onclick="javascript:history.go(-1)">
+</form>
+
+</body></html>
